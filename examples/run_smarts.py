1e9983d127834b0be84d9db108d01608231b7f11
Ming Zhou <kornbergfresnel@outlook.com>
2021-09-05 10:16:22 +0800

    update

--- examples/run_smarts.py
+++ examples/run_smarts.py
@@ -48,37 +48,37 @@ if __name__ == "__main__":
         )
     )
 
-    env.close()
-
-    # done = False
-    # cnt = 0
-    # obs = env.reset()
-    # while not done:
-    #     actions = {agent: space.sample() for agent, space in action_spaces.items()}
-    #     rets = env.step(actions)
-    #     rewards = rets[Episode.REWARD]
-    #     done = any(rets[Episode.DONE].values())
-    #     print("step: {}, actions: {}, rewards: {}".format(cnt, actions, rewards))
-    #     cnt += 1
     # env.close()
 
-    training_config = config["training"]
-    rollout_config = config["rollout"]
+    done = False
+    cnt = 0
+    obs = env.reset()
+    while not done:
+        actions = {agent: space.sample() for agent, space in action_spaces.items()}
+        rets = env.step(actions)
+        rewards = rets[Episode.REWARD]
+        done = any(rets[Episode.DONE].values())
+        print("step: {}, actions: {}, rewards: {}".format(cnt, actions, rewards))
+        cnt += 1
+    env.close()
 
-    training_config["interface"]["observation_spaces"] = observation_spaces
-    training_config["interface"]["action_spaces"] = action_spaces
+    # training_config = config["training"]
+    # rollout_config = config["rollout"]
 
-    run(
-        group=config["group"],
-        name=config["name"],
-        env_description=env_desc,
-        agent_mapping_func=lambda agent: "share",
-        training=training_config,
-        algorithms=config["algorithms"],
-        # rollout configuration for each learned policy model
-        rollout=rollout_config,
-        evaluation=config.get("evaluation", {}),
-        global_evaluator=config["global_evaluator"],
-        dataset_config=config.get("dataset_config", {}),
-        parameter_server=config.get("parameter_server", {}),
-    )
+    # training_config["interface"]["observation_spaces"] = observation_spaces
+    # training_config["interface"]["action_spaces"] = action_spaces
+
+    # run(
+    #     group=config["group"],
+    #     name=config["name"],
+    #     env_description=env_desc,
+    #     agent_mapping_func=lambda agent: "share",
+    #     training=training_config,
+    #     algorithms=config["algorithms"],
+    #     # rollout configuration for each learned policy model
+    #     rollout=rollout_config,
+    #     evaluation=config.get("evaluation", {}),
+    #     global_evaluator=config["global_evaluator"],
+    #     dataset_config=config.get("dataset_config", {}),
+    #     parameter_server=config.get("parameter_server", {}),
+    # )
