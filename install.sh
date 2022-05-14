5935f7dec95822f373f5fc6e4030494a1ac96b48
Ming Zhou <kornbergfresnel@outlook.com>
2022-05-14 06:00:04 +0000

    fix dependencies

---
+++ install.sh
@@ -0,0 +1,25 @@
+# import tsinghua conda mirror
+conda config --set show_channel_urls yes
+cat << EOF > $HOME/.condarc
+channels:
+  - defaults
+show_channel_urls: true
+default_channels:
+  - https://mirrors.tuna.tsinghua.edu.cn/anaconda/pkgs/main
+  - https://mirrors.tuna.tsinghua.edu.cn/anaconda/pkgs/r
+  - https://mirrors.tuna.tsinghua.edu.cn/anaconda/pkgs/msys2
+custom_channels:
+  conda-forge: https://mirrors.tuna.tsinghua.edu.cn/anaconda/cloud
+  msys2: https://mirrors.tuna.tsinghua.edu.cn/anaconda/cloud
+  bioconda: https://mirrors.tuna.tsinghua.edu.cn/anaconda/cloud
+  menpo: https://mirrors.tuna.tsinghua.edu.cn/anaconda/cloud
+  pytorch: https://mirrors.tuna.tsinghua.edu.cn/anaconda/cloud
+  pytorch-lts: https://mirrors.tuna.tsinghua.edu.cn/anaconda/cloud
+  simpleitk: https://mirrors.tuna.tsinghua.edu.cn/anaconda/cloud
+EOF
+conda clean -i
+conda install pytorch cudatoolkit=11.3 -c pytorch -y
+pip config set global.index-url https://mirror.sjtu.edu.cn/pypi/web/simple
+pip install --upgrade pip
+pip install -r requirements.txt
+#./install_sc2.sh
